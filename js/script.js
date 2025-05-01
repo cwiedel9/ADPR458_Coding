5d2124f47355583bdbf718059323d26923ab3349
Caleb Wiedel <caleb@beunanimous.com>
2025-05-01 10:11:12 -0500

    added slick slider

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
-const toggleBtn = document.getElementById("toggle-button");
-const navListItems = document.getElementById("nav-menu-list");
+$(".slider").slick({
 
-toggleBtn.addEventListener("click", function () {
-  navListItems.classList.toggle("active");
+  // normal options
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  pauseOnFocus: false,
+  pauseOnHover: false,
+
+  // the magic
+  responsive: [{
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+      }
+
+    }, {
+
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        dots: true,
+        autoplay: true
+      }
+
+    }, {
+
+      breakpoint: 300,
+      settings: "unslick" // destroys slick
+
+    }]
 });
